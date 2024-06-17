f0457127cd9504362ff5103eda710aa06a8ad696
YoonJungHae <yoongomtang@naver.com>
2024-06-17 16:06:46 +0900

    no message

--- spring_day_1/src/main/java/com/multi/spring/member/model/dao/MemberDAO.java
+++ spring_day_1/src/main/java/com/multi/spring/member/model/dao/MemberDAO.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.multi.spring.member.model.dto.MemberDTO;
 
 @Repository
+//@Repository 어노테이션은 Spring 프레임워크에서 데이터 액세스 계층(DAO)을 나타내는 데 사용됩니다. 
 public class MemberDAO {
 	public void insert(MemberDTO memberDTO) {
 		// TODO Auto-generated method stub
